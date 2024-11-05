6c9d1b72925cf3ef27353cc0b2acee6efb8704ac
Tipexe667 <tprayez@gmail.com>
2024-11-05 10:01:03 +0100

    Update page.tsx

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "components/ui/button"
+import { Button } from "./components/ui/button"
 import { Star, Truck } from 'lucide-react'
 
 export default function ProductPage({ params }: { params: { id: string } }) {
